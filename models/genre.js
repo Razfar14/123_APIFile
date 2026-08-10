5ad58cd61a3f24d070222780000a94173d0ab810
Razfar14 <rajarangkuty14@gmail.com>
2026-08-10 11:03:29 +0700

    feat: initialize database models and implement image upload middleware

--- models/genre.js
+++ models/genre.js
@@ -5,9 +5,29 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true
         },
-        title: {
+        nama: {
             type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: true
         }
+    },
+    {
+        tableName: 'genre',
+        timestamps: true
     });
+
+    Genre.associate = (models) => {
+        Genre.belongsToMany(models.Komik, {
+            through: 'komik_genre',
+            foreignKey: 'genre_id',
+            otherKey: 'komik_id',
+            as: 'komik'
+        });
+    };
+
     return Genre;
 };
