0f895d19ed2a456026f98e35e95f153bcdd0fe51
Razfar14 <rajarangkuty14@gmail.com>
2026-08-10 11:03:26 +0700

    feat: implement auth and upload middleware and define database models for Komik, Penulis, and Genre

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,22 +4,21 @@ const authMiddleware = (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
         if (!authHeader) {
-            return res.status(401).json
-                ({
-                    error: 'Authorization tidak ditemukan'
-                });
+            return res.status(401).json({
+                message: 'Authorization tidak ditemukan'
+            });
         }
 
         const token = authHeader.split(' ')[1];
         if (!token) {
-            return res.status(401).json
-                ({
-                    error: 'Token tidak valid'
-                });
+            return res.status(401).json({
+                message: 'Token tidak valid'
+            });
         }
 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+        req.penulis = decoded;
         req.user = decoded;
         next();
     } catch (error) {
